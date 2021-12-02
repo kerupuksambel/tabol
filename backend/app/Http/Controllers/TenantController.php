8a7e6f12ae87c238bfa65d69c26d20b3d41fbbee
Ammar Alifian Fahdan <kerupuksambel.2000@gmail.com>
2021-12-03 05:41:36 +0700

    feat: backend - home done

--- backend/app/Http/Controllers/TenantController.php
+++ backend/app/Http/Controllers/TenantController.php
@@ -14,14 +14,16 @@ class TenantController extends Controller
                 'user_id' => 1,
                 'nama' => 'Tambal Ban 1',
                 'deskripsi' => 'Jasa tambal ban daerah Bandung, Tulungagung, Jawa Timur',
-                'photo_url' => 'https://via.placeholder.com/150'
+                'photo_url' => 'https://via.placeholder.com/150',
+                'rating' => 3.2
             ],
             [
                 'id' => 2,
                 'user_id' => 2,
                 'nama' => 'Tambal Ban kedua',
                 'deskripsi' => 'Jasa tambal ban daerah Besuki, Tulungagung, Jawa Timur',
-                'photo_url' => 'https://via.placeholder.com/150/0000FF'
+                'photo_url' => 'https://via.placeholder.com/150/',
+                'rating' => 4.3
             ],
         ]);
     }
